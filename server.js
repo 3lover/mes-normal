7bc882f088404e4e68fbf51513b2530c08e61466
Glitch (husky-healthy-stone) <none>
2020-07-18 23:25:28 +0000

    😄💅 Checkpoint ./server.js:18138957/403 ./lib/definitions.js:18138957/118

--- server.js
+++ server.js
@@ -4646,7 +4646,7 @@ var maintainloop = (() => {
                 if (bots.length < c.BOTS) {
                     let o = new Entity(room.random());
                     o.color = 17;
-                    o.define(ran.dice(80)) ? ran.choose([Class.botrammer, Class.botgun]) : ran.choose([Class.botrammer, Class.botgun]);
+                    o.define(Class.botrammer);
                     o.define(Class.basic);
                     o.name += ran.chooseBotName();
                     o.refreshBodyAttributes();
