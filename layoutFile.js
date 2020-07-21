75740124084032b83bbc9159b6a82733a800b815
Glitch (husky-healthy-stone) <none>
2020-07-21 00:14:28 +0000

    👨‍❤️‍💋‍👨🏡 Checkpoint ./config.json:18138957/626 ./layout file:18138957/440

---
+++ layoutFile.js
@@ -0,0 +1,7 @@
+    [ "roid", "norm", "norm", "norm","norm", "norm", "roid"],
+    [ "norm", "rock", "rock", "rock","rock", "rock", "norm"],
+    [ "norm", "rock", "nest", "nest","nest", "rock", "norm"],
+    [ "norm", "rock", "nest", "roid","nest", "rock", "norm"],
+    [ "norm", "rock", "nest", "nest","nest", "rock", "norm"],
+    [ "norm", "rock", "rock", "rock","rock", "rock", "norm"],
+    [ "roid", "norm", "norm", "norm","norm", "norm", "roid"]
